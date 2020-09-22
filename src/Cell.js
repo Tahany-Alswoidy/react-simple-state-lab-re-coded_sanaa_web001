869176b6fe2678d7ae35a4d08fbc9055387e526d
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-22 10:04:53 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-export class Cell extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      color: props.value,
-    };
-  }
-  handleClick = () => {
-    this.setState({ color: "#333" });
-  };
+export default class Cell extends Component {    
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: this.props.value
+        }
+    }
 
+    updateColorState = () => {
+        this.setState({
+            color: '#333'
+        })
+    }
 
-  render() {
-    return (
-      <div
-        className="cell"
-        style={{ backgroundColor: this.state.color }}
-        onClick={this.handleClick}
-      ></div>
-    );
-  }
-}
-
-export default Cell;
+    render() {
+        return (
+            <div  onClick={this.updateColorState} className="cell" style={{backgroundColor: this.state.color}}></div>
+        )
+    } 
